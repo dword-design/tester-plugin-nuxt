fa05fa9e61a7dbfc88f306ed3052734a0860c315
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-03-13 01:16:59 +0100

    fix: use prod mode

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export default (config = {}) => ({
     return this.nuxt.close()
   },
   async before() {
-    this.nuxt = new Nuxt({ dev: true, ...config })
+    this.nuxt = new Nuxt(config)
     await new Builder(this.nuxt).build()
     await this.nuxt.listen()
   },
