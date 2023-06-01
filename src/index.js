23faa9f857892dcff14e88e46923ffa523c09a0c
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-06-01 22:22:05 +0000

    fix: foo

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ export default () => ({
   async before() {
     await execaCommand('nuxt build', {
       env: { NODE_ENV: 'production', NUXT_TELEMETRY_DISABLED: 1 },
+      stdio: 'inherit',
     })
     this.nuxt = execaCommand('nuxt start', {
       env: { NODE_ENV: 'production', NUXT_TELEMETRY_DISABLED: 1 },
+      stdio: 'inherit',
     })
     await portReady(3000)
   },
